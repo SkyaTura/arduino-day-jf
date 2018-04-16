9a024b6fd7923ed523626e42d5ba0bc98b716715
Antonio Eduardo <antonio@skyatura.com.br>
2018-04-16 00:17:06 -0300

    ✨ Add contrast button

--- components/LCDDisplaySvg.vue
+++ components/LCDDisplaySvg.vue
@@ -4,13 +4,16 @@
       <feGaussianBlur in="SourceGraphic" stdDeviation="1.5"/>
     </filter>
     <image height="100%" width="100%" xlink:href="~/assets/LCD_8bits.svg" alt=""></image>
-    <foreignObject x="286" y="145.5" width="188" class="text" font-size="17">
+    <foreignObject x="286" y="145.5" width="188" class="text" font-size="17" :style="customStyles">
       <p xmlns="http://www.w3.org/1999/xhtml">
         <slot></slot>
       </p>
     </foreignObject>
-    <foreignObject x="230" y="160" font-size="17" class="text">
-      <button class="button" @click="onButton">C</button>
+    <foreignObject x="28" y="106" font-size="17">
+      <button class="button" @click="onButton"></button>
+    </foreignObject>
+    <foreignObject x="230" y="160" font-size="17">
+      <button class="contrast" @click="toggleContrast"></button>
     </foreignObject>
     <ellipse rx="5" ry="2.5" cx="186" cy="151.5" :opacity="on" class="led-light led-on"></ellipse>
     <ellipse rx="5" ry="2.5" cx="186" cy="151.5" :opacity="on" class="led-light led-on"></ellipse>
@@ -38,9 +41,28 @@
         default: 0,
       },
     },
+    data () {
+      return {
+        currentContrast: 2,
+        availableContrast: [0.1, 0.3, 0.6, 0.9],
+      }
+    },
+    computed: {
+      customStyles () {
+        const contrast = this.availableContrast[this.currentContrast]
+        return {
+          color: `rgba(0,0,0,${contrast}`,
+        }
+      },
+    },
     methods: {
       onButton () {
         this.$emit('click')
+      },
+      toggleContrast () {
+        const { availableContrast, currentContrast } = this
+        const maxIndex = availableContrast.length - 1
+        this.currentContrast = currentContrast >= maxIndex ? 0 : currentContrast + 1
       },
     },
   }
@@ -50,10 +72,11 @@
   @import '~/assets/fonts/RepetitionScrolling.css';
 
   .text {
-    color: rgba(0, 0, 0, .6);
+    color: rgba(0, 0, 0, .6));
     font-family: Repetition-Scrolling, monospace;
     line-height: .88;
     transform: translateY(1px);
+    transition: color .2s;
     word-break: break-all;
     white-space: pre-wrap;
   }
@@ -68,12 +91,18 @@
     fill: greenyellow;
   }
 
-  .button {
+  .button, .contrast {
     background: none;
     border: none;
     color: transparent;
     cursor: pointer;
-    height: 30px;
+    height: 14px;
+    width: 14px;
     outline: none;
+  }
+
+  .contrast {
+    width: 24px;
+    height: 30px;
   }
 </style>
